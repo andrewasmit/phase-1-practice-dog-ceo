50d5b49c5a539de62166ae857d4e241a460f18f1
andrew <andrewasmit@hotmail.com>
2022-11-05 19:41:48 -0500

    first commit. 3/4 done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
 console.log('%c HI', 'color: firebrick')
+
+
+const dogDiv = document.querySelector('#dog-image-container');
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+const breedUrl = 'https://dog.ceo/api/breeds/list/all';
+const breedDiv = document.querySelector('#dog-breeds');
+
+fetch(imgUrl)
+.then(res=>res.json())
+.then(data=>handleImgs(data));
+
+
+function handleImgs(e){
+    // console.log(e.message);
+    let imgArr = e.message.map(img=>{
+        return `<img src = ${img}>`
+    })
+    // console.log(imgArr);
+    imgArr.forEach(elem=>{
+        dogDiv.innerHTML += elem;
+    })
+}
+
+
+fetch(breedUrl)
+.then(res=>res.json())
+.then(data=>handleBreeds(data));
+
+function handleBreeds(e){
+    const breedObj = Object.keys(e.message);
+    // console.log(breedObj);
+    const breedArr = breedObj.map(breed=>{
+        let li = `<li class= "breed-li">${breed}</li>`
+        return li;
+    })
+    // console.log(breedArr);
+    breedArr.forEach(elem=>{
+        breedDiv.innerHTML += elem;
+    })
+}
+
+breedDiv.addEventListener('click', function(e){
+    let target = e.target;
+    // console.log(target);
+    target.style.color = 'red';
+})
